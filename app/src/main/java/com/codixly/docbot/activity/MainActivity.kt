9618ff3e97a82127c9cf6bdfd2ebb2e317857bdb
Jaydip Tank <jaydiptank2004@gmail.com>
2025-06-10 14:48:54 +0530

    10-06-2025:14:48

---
+++ app/src/main/java/com/codixly/docbot/activity/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.codixly.docbot.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.codixly.docbot.R
+import com.codixly.docbot.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val NavController = findNavController(R.id.fragmentContainerView)
+        val bottomnav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomnav.setupWithNavController(NavController)
+
+    }
+}
