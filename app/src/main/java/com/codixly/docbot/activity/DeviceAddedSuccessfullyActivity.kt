f032ebe503b23d3659f48fe941c08be930f22744
Jaydip Tank <95556824+Jaydip04@users.noreply.github.com>
2025-06-11 13:23:04 +0530

    11-06-2025 01:23 PM

--- app/src/main/java/com/codixly/docbot/activity/DeviceAddedSuccessfullyActivity.kt
+++ app/src/main/java/com/codixly/docbot/activity/DeviceAddedSuccessfullyActivity.kt
@@ -1,6 +1,9 @@
 package com.codixly.docbot.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +20,10 @@ class DeviceAddedSuccessfullyActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@DeviceAddedSuccessfullyActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
--- app/src/main/java/com/codixly/docbot/activity/SignUpActivity.kt
+++ app/src/main/java/com/codixly/docbot/activity/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.codixly.docbot.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
 import android.util.Log
@@ -50,78 +51,82 @@ class SignUpActivity : AppCompatActivity() {
         }
 
         binding.btnSignIn.setOnClickListener {
-            val username = binding.etClientId.text.toString().trim()
-            val password = binding.etPassword.text.toString().trim()
-
-            if (username.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            val request = LoginRequest(username, password)
-
-            ApiClient.instance.loginUser(request).enqueue(object : retrofit2.Callback<LoginResponse> {
-                override fun onResponse(
-                    call: retrofit2.Call<LoginResponse>,
-                    response: retrofit2.Response<LoginResponse>
-                ) {
-                    if (response.isSuccessful) {
-                        val res = response.body()
-                        if (res?.status == true) {
-                            Toast.makeText(this@SignUpActivity, "Login Successful", Toast.LENGTH_SHORT).show()
-                            // Save token or user data if needed
-                            val sharedPref = getSharedPreferences("user_data", MODE_PRIVATE)
-                            with(sharedPref.edit()) {
-                                putString("token", res.customer?.token)
-                                putString("name", res.customer?.name)
-                                putString("username", res.customer?.username)
-                                putString("email", res.customer?.email)
-                                putString("mobile", res.customer?.mobile)
-                                putString("machine_id", res.customer?.machine_id)
-                                putString("bluetooth_id", res.machineData?.blutooth_id)
-                                apply()
-                            }
-//                            Get Data from SharedPreferences
-//                            val sharedPref = getSharedPreferences("user_data", MODE_PRIVATE)
-                            val token = sharedPref.getString("token", null)
-                            val name = sharedPref.getString("name", null)
-//                            val username = sharedPref.getString("username", null)
-                            val email = sharedPref.getString("email", null)
-//                            val mobile = sharedPref.getString("mobile", null)
-//                            val machineId = sharedPref.getString("machine_id", null)
-//                            val bluetoothId = sharedPref.getString("bluetooth_id", null)
-
-                            Log.d("StoredData", "Token: $token")
-                            Log.d("StoredData", "Name: $name")
-                            Log.d("StoredData", "Email: $email")
-
-//                            Clear Data on Logout
-//                            val sharedPref = getSharedPreferences("user_data", MODE_PRIVATE)
-//                            sharedPref.edit().clear().apply()
-
-
-                            Log.d("LoginResponse", "Data saved to SharedPreferences")
-                            Log.d("LoginResponse", "Result: ${res}")
-                            Log.d("LoginResponse", "Customer Name: ${res.customer?.name}")
-                            Log.d("LoginResponse", "Username: ${res.customer?.username}")
-                            Log.d("LoginResponse", "Email: ${res.customer?.email}")
-                            Log.d("LoginResponse", "Mobile: ${res.customer?.mobile}")
-                            Log.d("LoginResponse", "Token: ${res.customer?.token}")
-                            Log.d("LoginResponse", "Machine ID: ${res.customer?.machine_id}")
-                            Log.d("LoginResponse", "Bluetooth ID: ${res.machineData?.blutooth_id}")
-//                            val token = res.customer?.token
-                        } else {
-                            Toast.makeText(this@SignUpActivity, res?.message ?: "Login failed", Toast.LENGTH_SHORT).show()
-                        }
-                    } else {
-                        Toast.makeText(this@SignUpActivity, "Server error", Toast.LENGTH_SHORT).show()
-                    }
-                }
-
-                override fun onFailure(call: retrofit2.Call<LoginResponse>, t: Throwable) {
-                    Toast.makeText(this@SignUpActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
-                }
-            })
+            val intent = Intent(this@SignUpActivity, BluetoothScanScreenActivity::class.java)
+            startActivity(intent)
+            finish()
+//            val username = binding.etClientId.text.toString().trim()
+//            val password = binding.etPassword.text.toString().trim()
+//
+//            if (username.isEmpty() || password.isEmpty()) {
+//                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+//                return@setOnClickListener
+//            }
+//
+//            val request = LoginRequest(username, password)
+//
+//            ApiClient.instance.loginUser(request).enqueue(object : retrofit2.Callback<LoginResponse> {
+//                override fun onResponse(
+//                    call: retrofit2.Call<LoginResponse>,
+//                    response: retrofit2.Response<LoginResponse>
+//                ) {
+//                    if (response.isSuccessful) {
+//                        val res = response.body()
+//                        if (res?.status == true) {
+//
+////                            Toast.makeText(this@SignUpActivity, "Login Successful", Toast.LENGTH_SHORT).show()
+////                            // Save token or user data if needed
+////                            val sharedPref = getSharedPreferences("user_data", MODE_PRIVATE)
+////                            with(sharedPref.edit()) {
+////                                putString("token", res.customer?.token)
+////                                putString("name", res.customer?.name)
+////                                putString("username", res.customer?.username)
+////                                putString("email", res.customer?.email)
+////                                putString("mobile", res.customer?.mobile)
+////                                putString("machine_id", res.customer?.machine_id)
+////                                putString("bluetooth_id", res.machineData?.blutooth_id)
+////                                apply()
+////                            }
+//////                            Get Data from SharedPreferences
+//////                            val sharedPref = getSharedPreferences("user_data", MODE_PRIVATE)
+////                            val token = sharedPref.getString("token", null)
+////                            val name = sharedPref.getString("name", null)
+//////                            val username = sharedPref.getString("username", null)
+////                            val email = sharedPref.getString("email", null)
+//////                            val mobile = sharedPref.getString("mobile", null)
+//////                            val machineId = sharedPref.getString("machine_id", null)
+//////                            val bluetoothId = sharedPref.getString("bluetooth_id", null)
+////
+////                            Log.d("StoredData", "Token: $token")
+////                            Log.d("StoredData", "Name: $name")
+////                            Log.d("StoredData", "Email: $email")
+////
+//////                            Clear Data on Logout
+//////                            val sharedPref = getSharedPreferences("user_data", MODE_PRIVATE)
+//////                            sharedPref.edit().clear().apply()
+////
+////
+////                            Log.d("LoginResponse", "Data saved to SharedPreferences")
+////                            Log.d("LoginResponse", "Result: ${res}")
+////                            Log.d("LoginResponse", "Customer Name: ${res.customer?.name}")
+////                            Log.d("LoginResponse", "Username: ${res.customer?.username}")
+////                            Log.d("LoginResponse", "Email: ${res.customer?.email}")
+////                            Log.d("LoginResponse", "Mobile: ${res.customer?.mobile}")
+////                            Log.d("LoginResponse", "Token: ${res.customer?.token}")
+////                            Log.d("LoginResponse", "Machine ID: ${res.customer?.machine_id}")
+////                            Log.d("LoginResponse", "Bluetooth ID: ${res.machineData?.blutooth_id}")
+////                            val token = res.customer?.token
+//                        } else {
+//                            Toast.makeText(this@SignUpActivity, res?.message ?: "Login failed", Toast.LENGTH_SHORT).show()
+//                        }
+//                    } else {
+//                        Toast.makeText(this@SignUpActivity, "Server error", Toast.LENGTH_SHORT).show()
+//                    }
+//                }
+//
+//                override fun onFailure(call: retrofit2.Call<LoginResponse>, t: Throwable) {
+//                    Toast.makeText(this@SignUpActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+//                }
+//            })
         }
 
     }
