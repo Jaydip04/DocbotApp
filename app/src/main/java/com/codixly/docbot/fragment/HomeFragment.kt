fdffdea98b0a0dd0c4b608a372b3589e445838be
Jaydip Tank <jaydiptank2004@gmail.com>
2025-06-12 21:28:03 +0530

    12-06-2025

--- app/src/main/java/com/codixly/docbot/fragment/HomeFragment.kt
+++ app/src/main/java/com/codixly/docbot/fragment/HomeFragment.kt
@@ -1,21 +1,35 @@
 package com.codixly.docbot.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.codixly.docbot.R
+import com.codixly.docbot.activity.NotificationActivity
+import com.codixly.docbot.activity.DeviceInfoActivity
+import com.codixly.docbot.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        // 🔔 Set click listener on notification button/view
+        binding.notification.setOnClickListener {
+            val intent = Intent(requireContext(), NotificationActivity::class.java)
+            startActivity(intent)
+        }
+        binding.monitoring.setOnClickListener{
+            val intent=Intent(requireContext(),DeviceInfoActivity::class.java)
+            startActivity(intent)
+        }
+
+        return binding.root
     }
 
 
