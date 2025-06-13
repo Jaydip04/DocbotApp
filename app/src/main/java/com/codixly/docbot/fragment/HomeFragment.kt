888508b8068e4602d50daa4ed0f21ce2bd06fdf4
Jaydip Tank <95556824+Jaydip04@users.noreply.github.com>
2025-06-13 09:01:58 +0530

    13-06-2025 08:56 PM

--- app/src/main/java/com/codixly/docbot/fragment/HomeFragment.kt
+++ app/src/main/java/com/codixly/docbot/fragment/HomeFragment.kt
@@ -3,8 +3,6 @@ package com.codixly.docbot.fragment
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
